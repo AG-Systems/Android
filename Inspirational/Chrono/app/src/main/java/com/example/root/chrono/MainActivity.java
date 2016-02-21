e7a34305aaa33716f3270ddbf509c32f515bdfc8
Auriga <area51alienware@gmail.com>
2016-02-20 23:53:48 -0800

    Cant get location to work.

--- Inspirational/Chrono/app/src/main/java/com/example/root/chrono/MainActivity.java
+++ Inspirational/Chrono/app/src/main/java/com/example/root/chrono/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.root.chrono;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                textView.append("\n " +location.getLatitude() + " "+location.getLongitude());
             }
 
             @Override
@@ -47,25 +50,39 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String provider) {
-
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
             }
         };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
+    }
+
+    private void configureButton()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{
-                   Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                  Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.INTERNET
             }, 3 );
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-
-        locationManager.requestLocationUpdates("gps", 5000, 30, locationListener);
+        else {
+            configureButton();
+        }
+        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grandResults)
+    {
+       switch (requestCode)
+       {
+           case 3:
+               if(grandResults.length>0 && grandResults[0] == PackageManager.PERMISSION_GRANTED)
+                   configureButton();
+               return;
+       }
+    }
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view)
+        {
+            locationManager.requestLocationUpdates("gps", 5000, 30, locationListener);
+        }
+    });
     }
 }
