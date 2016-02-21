6dde25f7b44d48eb1b41e8ffa67cbb6b12a22694
Auriga <area51alienware@gmail.com>
2016-02-20 22:57:30 -0800

    Adding some java!

--- Inspirational/Chrono/app/src/main/java/com/example/root/chrono/MainActivity.java
+++ Inspirational/Chrono/app/src/main/java/com/example/root/chrono/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.root.chrono;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private Button button;
+    private LocationManager locationManager;
+    private TextView textView;
+    private LocationListener locationListener;
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
