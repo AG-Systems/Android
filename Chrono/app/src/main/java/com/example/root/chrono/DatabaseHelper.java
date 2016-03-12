ab5aa338d63c76c286e89157118278ab913a2de8
Auriga <area51alienware@gmail.com>
2016-03-11 20:36:38 -0800

    Learning a lot of things.

--- Chrono/app/src/main/java/com/example/root/chrono/DatabaseHelper.java
+++ Chrono/app/src/main/java/com/example/root/chrono/DatabaseHelper.java
@@ -8,8 +8,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by root on 3/9/2016.
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    // String name, SQLiteDatabase.CursorFactory factory, int version
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getReadableDatabase();
     }
     public static final String DATABASE_NAME = "main.db";
     public static final String TABLE_NAME = "main_table";
@@ -19,11 +21,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_4 = "MARKS";
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KAY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER) ");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
     }
 }
