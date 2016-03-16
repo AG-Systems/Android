de0219909ffbbe54e6de245c64101301875883f5
Auriga <area51alienware@gmail.com>
2016-03-15 20:22:57 -0700

    Going to work with databases now.

--- Chrono/app/src/main/java/com/example/root/chrono/MapsActivity.java
+++ Chrono/app/src/main/java/com/example/root/chrono/MapsActivity.java
@@ -9,13 +9,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.root.chrono.MainActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    DatabaseHelper myDb;
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        myDb = new DatabaseHelper(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
