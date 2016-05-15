d8b1564468ec48da1f8659b56162a13657ee2e9e
Auriga <area51alienware@gmail.com>
2016-05-15 13:25:35 -0700

    Switched the name

---
+++ Chrono/app/src/main/java/com/example/root/chrono/DBHandler.java
@@ -0,0 +1,9 @@
+package com.example.root.chrono;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ * Created by root on 5/15/2016.
+ */
+public class DBHandler {
+}
--- Chrono/app/src/main/java/com/example/root/chrono/Database.java
+++
@@ -1,9 +0,0 @@
-package com.example.root.chrono;
-
-/**
- * Created by root on 5/15/2016.
- */
-public class Database
-{
-    
-}
